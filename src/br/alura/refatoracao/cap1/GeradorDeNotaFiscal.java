db0fa1016e93ea939ef528f877fba4264b0668e5
rogeriop <pinheiroxrogeriop>
2016-04-14 14:48:47 -0300

    fim 01 - Extraindo metodos e classes

--- src/br/alura/refatoracao/cap1/GeradorDeNotaFiscal.java
+++ src/br/alura/refatoracao/cap1/GeradorDeNotaFiscal.java
@@ -4,6 +4,17 @@ public class GeradorDeNotaFiscal {
 
 	public NotaFiscal gera(Fatura fatura) {
 
+		NotaFiscal nf = geraNotaFiscal(fatura);
+
+		new Email().enviaEmailNotaFiscal(nf);
+		
+		new NotaFiscalDao().salvaNoBanco(nf);
+
+		return nf;
+	}
+
+	private NotaFiscal geraNotaFiscal(Fatura fatura) {
+
 		// calcula valor do imposto
 		double valor = fatura.getValorMensal();
 		double imposto = 0;
@@ -18,22 +29,6 @@ public class GeradorDeNotaFiscal {
 		}
 		
 		NotaFiscal nf = new NotaFiscal(valor, imposto);
-
-		// envia email
-		String msgDoEmail = "Caro cliente,<br/>";
-		msgDoEmail += "É com prazer que lhe avisamos que sua nota fiscal foi "
-				+ "gerada no valor de " + nf.getValorLiquido() + ".<br/>";
-		msgDoEmail += "Acesse o site da prefeitura e faça o download.<br/><br/>";
-		msgDoEmail += "Obrigado!";
-		
-		System.out.println(msgDoEmail);
-		
-		// salva no banco
-		String sql = "insert into notafiscal (cliente, valor)"+
-					 "values (?," + nf.getValorLiquido() + ")";
-		
-		System.out.println("Salvando no banco" + sql);
-
 		return nf;
 	}
 }
